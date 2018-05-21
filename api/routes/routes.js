2dda4cb13db52e0ee677208e53d36bb6fb9f796e
kostyny <sergiomdcoliveira@gmail.com>
2018-05-21 12:21:12 +0100

    remover comentários

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -5,17 +5,6 @@ const path = require('path')
 
 const user = require('../controllers/user.controller')
 
-/*router.get('/index', (req, res)=>{
-    res.sendFile(global.root + '/views/html/' + 'index.html')
-})*/
-/*
-router.get('/indexl', function(req, res) {
-    //leitura do ficheiro estático - view do user
-    res.sendFile( path.join(__dirname, '../views/html/') + 'index.html');
-});
-*/
-
-
 router.get('/posts', user.userController.getAllUsers)
 router.get('/post/:id', user.userController.findOneUser)
 router.delete('/post/:id', user.userController.deleteUser)
